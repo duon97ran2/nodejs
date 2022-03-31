e6f8b1ed9d922cef3075b49ad2c296aaacad50b2
duon97ran2 <duongtaph13276@fpt.edu.vn>
2022-03-31 23:24:50 +0700

    update app.js

--- src/app.js
+++ src/app.js
@@ -1,26 +1,3 @@
-// const http = require("http");
-
-// const server = http.createServer((req,res)=>{
-//   console.log(req.url);
-//   const url = req.url;
-//   if(url==="/api/products"){
-//     const data =[
-//       {id:1, name:"Product A"},
-//       {id:2, name:"Product B"}
-//     ];
-//     res.end(JSON.stringify(data))
-//   }else if(url === "/api/posts"){
-//     console.log("API post");
-//   }else{
-//     res.setHeader("Content-Type","text/html");
-//     res.write("<html><body><h1>HOME PAGE</h1></body></html>");
-//     res.end();
-//   }
-// });
-// const PORT = 4000;
-// server.listen(PORT,()=>{
-//   console.log("Server is running",PORT);
-// });
 import express from "express";
 import cors from "cors";
 import productRoute from "./routes/product.js";
@@ -39,10 +16,11 @@ dotenv.config({ path: __dirname + "/configs/.env" });
 
 const app = express();
 const swaggerJSDOcs = YAML.load(__dirname + "/configs/api.yaml");
-app.use(cors());
 app.use(morgan('tiny'));
 app.use(express.json());
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerJSDOcs));
+
+app.use(cors());
 
 app.use("/api", productRoute);
 app.use("/api", categoryRoute);
