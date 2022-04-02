a20a57ad2bd3adbe73b996f513af3714be9ed6e0
duon97ran2 <duongtaph13276@fpt.edu.vn>
2022-04-02 15:54:40 +0700

    add middleware

--- src/routes/product.js
+++ src/routes/product.js
@@ -11,6 +11,6 @@ router.get("/products", list);
 router.get("/products/:id", getOne);
 router.delete("/products/:id", isAuth, isExist, isAdmin, remove);
 router.put("/products/:id", isAuth, isExist, isAdmin, update);
-router.post("/products", create);
+router.post("/products", isAuth, isExist, isAdmin, create);
 
 export default router;
