78c2e6884408a084065ac9627f3159a75495c9ed
duon97ran2 <duongtaph13276@fpt.edu.vn>
2022-04-02 15:43:28 +0700

    update product model

--- src/models/product.js
+++ src/models/product.js
@@ -12,7 +12,7 @@ const productSchema = mongoose.Schema({
     unique: true,
   },
   image: {
-    type: Array,
+    type: Object,
     default: "https://m.media-amazon.com/images/I/712yjQksyHL._AC_SY450_.jpg"
   },
   price: {
