88211986723e481e24e34529dae41b0abdabdde2
duon97ran2 <duongtaph13276@fpt.edu.vn>
2022-04-04 10:35:33 +0700

    add corsOptions

--- src/app.js
+++ src/app.js
@@ -19,7 +19,13 @@ const swaggerJSDOcs = YAML.load(__dirname + "/configs/api.yaml");
 app.use(morgan('tiny'));
 app.use(express.json());
 
-app.use(cors());
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  credentials: true,            //access-control-allow-credentials:true
+  optionSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
 
 app.use("/api/swaggers", swaggerUI.serve, swaggerUI.setup(swaggerJSDOcs));
 app.use("/api", productRoute);
