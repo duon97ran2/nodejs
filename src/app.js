3e4f2c6a86e3b520a865eb84ea4348b14ca3189b
duon97ran2 <duongtaph13276@fpt.edu.vn>
2022-04-17 14:10:31 +0700

    update cors origin

--- src/app.js
+++ src/app.js
@@ -22,7 +22,7 @@ app.use(morgan('tiny'));
 app.use(express.json({ limit: "50mb" }));
 
 const corsOptions = {
-  origin: 'http://localhost:3000',
+  origin: 'https://redex-typescript-assignment-9moxbooju-duon97ran2.vercel.app/',
   credentials: true,            //access-control-allow-credentials:true
   optionSuccessStatus: 200
 };
